462f3174ed730368aabcaffd0819918ed96b665b
neothermic <michaelkew@hotmail.com>
2017-10-04 18:43:48 +0000

    fixed a fail

--- config_files/inst_network.sh
+++ config_files/inst_network.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Installing WiFi configs"
-sudo mv ~/theatrix/config_files/interfaces /etc/network/interfaces
-sudo mv ~/theatrix/config_files/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+sudo cp ~/theatrix/config_files/interfaces /etc/network/interfaces
+sudo cp ~/theatrix/config_files/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 echo "Install complete, reboot to connect"
