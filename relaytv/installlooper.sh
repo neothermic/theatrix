a3b9e63640a7cb3e36a239376a090efdc6fa57a9
neothermic <michaelkew@hotmail.com>
2017-10-05 19:37:25 +0000

    updated installer to use dedicated FTP server ftpd instead of sftp to increase compatibility with windows systems

--- relaytv/installlooper.sh
+++ relaytv/installlooper.sh
@@ -7,10 +7,16 @@ echo "Installing script into crontab"
 
 line="@reboot screen -dmS loop bash -i ~/theatrix/relaytv/loopScript.sh"
 (crontab -u pi -l; echo "$line" ) | crontab -u pi -
-echo "Creating content folder at /home/content/"
+echo "Installing pure-ftpd"
+sudo apt-get install pure-ftpd
+echo "Setting up ftp users"
+sudo groupadd ftpgroup
+sudo useradd ftpuser -g ftpgroup -s /sbin/nologin -d /dev/null
 sudo mkdir /home/content
-echo "Creating content user and assigning home folder"
-sudo useradd content
-sudo chown content:users /home/content
+sudo chown -R ftpuser:ftpgroup /home/content
+echo "Please enter a password for the new FTP user 'content'"
+sudo pure-pw useradd content -u ftpuser -g ftpgroup -d /home/content -m
+sudo pure-pw mkdb
+sudo ln -s /etc/pure-ftpd/conf/PureDB /etc/pure-ftpd/auth/60puredb
+sudo service pure-ftpd restart
 echo "Install complete, add content and reboot to start playback"
-echo "Run 'sudo passwd content' to set password for content manager"
