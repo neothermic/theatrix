8abf36e52c202f64ff786546f829d09ae608fdc7
neothermic <michaelkew@hotmail.com>
2017-10-11 21:59:58 +0000

    possibly broke the script

--- relaytv/installlooper.sh
+++ relaytv/installlooper.sh
@@ -15,8 +15,8 @@ mkdir ~/theatrix/content
 echo "Creating public/private keypair and registering it with theatrix1"
 ssh-keygen -t rsa -C "$HOSTNAME key for theatrix1"
 cat ~/.ssh/id_rsa.pub | ssh pi@theatrix1.local 'cat >> .ssh/authorized_keys'
-echo "Enter the name of the source folder for this unit"
-READ ftp_source
+echo Enter the name of the source folder for this unit
+read ftp_source
 (crontab -u pi -l; echo "@reboot sudo rsync -azvh --delete -e ssh pi@theatrix1.local:~/$ftpsource/ ~/theatrix/content"
 
 
