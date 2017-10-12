b3394de53e067ee0a10a5853dc0ee5ffdc817a32
neothermic <michaelkew@hotmail.com>
2017-10-12 02:31:00 +0100

    Reworked how rsync command is fired
    Put rsync command into the loopscript to ensure content is updated BEFORE the looper starts.

--- relaytv/installlooper.sh
+++ relaytv/installlooper.sh
@@ -8,18 +8,19 @@
 #cat ~/theatrix/relaytv/bashrc.ct >> ~/.bashrc  #appends contents of bashrc.ct to bashrc, creating the playall command as an alias
 
 #echo "Installing looper script into crontab"
-#line="@reboot screen -dmS loop bash -i ~/theatrix/relaytv/loopScript.sh"
+#line="@reboot screen -dmS loop bash -i ~/theatrix/content/loopScript.sh"
 #(crontab -u pi -l; echo "$line" ) | crontab -u pi -
 echo "Creating content folder at ~/theatrix/content/"
 mkdir ~/theatrix/content
 echo "Creating public/private keypair and registering it with theatrix1"
 ssh-keygen -t rsa -C "$HOSTNAME key for theatrix1"
 cat ~/.ssh/id_rsa.pub | ssh pi@theatrix1.local 'cat >> .ssh/authorized_keys'
-echo "Adding rsync command to cron @reboot"
+echo "Moving loopScript to ~/theatrix/content/ - This directory is gitignored"
+cp ~/theatrix/relaytv/loopScript.sh ~/theatrix/content/loopScript.sh
+echo "Adding rsync command to loopscript"
 echo Enter the name of the source folder for this unit:
 read ftp_source
-(crontab -u pi -l; echo "@reboot sudo rsync -azvh --delete -e ssh pi@theatrix1.local:/home/content/$ftpsource/ ~/theatrix/content" ) | crontab -u pi -
-
-
-
+echo 'sudo rsync -azvh --delete -e ssh pi@theatrix1.local:/home/content/$ftpsource/ ~/theatrix/content' >> ~/theatrix/content/loopScript.sh
+echo "Catting rest of loopscript together"
+cat ~/theatrix/relaytv/loopScript.ct >> ~/theatrix/content/loopScript.sh  #Appends remainder of original loopscript, now in to-cat file, to content script
 echo "Install complete, add content and reboot to start playback"
