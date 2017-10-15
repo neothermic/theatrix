26797460e414848ffeba745dfaaa8054c90adea5
NeoThermic <michaelkew@hotmail.com>
2017-10-15 17:44:37 +0100

    made aftermarket registration ignore strict host checking for ease of update

--- relaytv/register_ssh.sh
+++ relaytv/register_ssh.sh
@@ -1,5 +1,7 @@
 !#/bin/sh
 #Use only to register clients with a new FTP master server at theatrix1. Most common case would be if theatrix1 has been replaced
+echo "Telling SSH to ignore strict host checking" 
+cat ~/theatrix/relaytv/ssh_config.ct >> /etc/ssh/ssh_config  #appends line to stop strict host checking
 echo Creating public/private keypair and registering it with theatrix1, what is my hostname:
 read my_name
 ssh-keygen -t rsa -C "$my_name key for theatrix1"
