cb932c801a886b1118218312b0de836a875657e5
neothermic <michaelkew@hotmail.com>
2017-10-15 22:27:34 +0100

    updated script to require root
    Updated script to check for root, and corrected paths to function when script is running as root

--- relaytv/register_ssh.sh
+++ relaytv/register_ssh.sh
@@ -1,9 +1,15 @@
 !#/bin/sh
+ROOTUID="0"
 #Use only to register clients with a new FTP master server at theatrix1. Most common case would be if theatrix1 has been replaced
+if [ "$(id -u)" -ne "$ROOTUID" ] then
+	echo "This script requires root"
+	exit 1
+fi
+
 echo "Telling SSH to ignore strict host checking" 
-cat ~/theatrix/relaytv/ssh_config.ct >> /etc/ssh/ssh_config  #appends line to stop strict host checking
+cat home/pi/theatrix/relaytv/ssh_config.ct >> /etc/ssh/ssh_config  #appends line to stop strict host checking
 echo Creating public/private keypair and registering it with theatrix1, what is my hostname:
 read my_name
 ssh-keygen -t rsa -C "$my_name key for theatrix1"
-sudo cat ~/.ssh/id_rsa.pub | ssh pi@theatrix1.local 'cat >> .ssh/authorized_keys'
+sudo cat home/pi/.ssh/id_rsa.pub | ssh pi@theatrix1.local 'cat >> .ssh/authorized_keys'
 echo Registration complete - Try ssh pi@theatrix1.local to test passwordless access
